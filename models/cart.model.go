655913d7b1e207a328ce97b34f590f36d9514fc3
khris-xp <khrisbharmmano@gmail.com>
2024-02-16 12:19:17 +0700

    chore(cart): update cart model with topping and comment

--- models/cart.model.go
+++ models/cart.model.go
@@ -7,6 +7,8 @@ type Cart struct {
 	UserId    string    `json:"user_id" bson:"user_id"`
 	MenuId    string    `json:"menu_id" bson:"menu_id"`
 	Quantity  int       `json:"quantity" bson:"quantity"`
+	Topping   []string  `json:"topping" bson:"topping"`
+	Comment   string    `json:"comment" bson:"comment"`
 	Status    string    `json:"status" bson:"status" default:"pending"`
 	CreatedAt time.Time `json:"created_at" bson:"created_at"`
 	UpdatedAt time.Time `json:"updated_at" bson:"updated_at"`
